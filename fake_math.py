c3d9a9203e797eb293d026dd4b285d8db74e0466
nadezhdaigumenova <nadya.kyzlakova@mail.ru>
2024-07-02 10:10:27 +0700

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,5 @@
+def divide(first, second):
+    if second == 0:
+        return str('Ошибка')
+    return first / second
+
---
+++ true_math.py
@@ -0,0 +1,5 @@
+def divide(first, second):
+    if second == 0:
+        from math import inf
+        return inf
+    return first / second
